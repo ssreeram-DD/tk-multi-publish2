9e7cba1faaf8add3e54489972db1b7232dc77b9d
Garvit Verma <gverma@d2.com>
2018-03-12 16:20:10 +0530

    added a PRELOAD_SIGNALER to dialog which connects to on_drop, handled using SGTK_PUBLISH_PRELOAD_PATH environ variable in show_dialog

--- python/tk_multi_publish2/__init__.py
+++ python/tk_multi_publish2/__init__.py
@@ -9,9 +9,24 @@
 # not expressly granted therein are reserved by Shotgun Software Inc.
 
 import sgtk
+import os
 
 from . import base_hooks
 from . import util
+
+
+def _handle_sgtk_publish_preload_path(app, sgtk_publish_preload_path, PRELOAD_SIGNALER):
+    from sgtk.platform.qt import QtCore
+    folders = list()
+
+    for root, dirs, files in os.walk(sgtk_publish_preload_path):
+        if not dirs:
+            folders.append(root)
+
+    def on_start():
+        PRELOAD_SIGNALER.preload_signal.emit(folders)
+
+    QtCore.QTimer.singleShot(1000, on_start)
 
 def show_dialog(app):
     """
@@ -21,11 +36,17 @@ def show_dialog(app):
     """
     # defer imports so that the app works gracefully in batch modes
     from .dialog import AppDialog
+    from .dialog import PRELOAD_SIGNALER
 
     display_name = sgtk.platform.current_bundle().get_setting("display_name")
 
     # start ui
     app.engine.show_dialog(display_name, app, AppDialog)
+
+    # this will pre-populate publisher when it is run with --p flag.
+    sgtk_publish_preload_path = os.environ.get('SGTK_PUBLISH_PRELOAD_PATH')
+    if sgtk_publish_preload_path and os.path.isdir(sgtk_publish_preload_path):
+        _handle_sgtk_publish_preload_path(app, sgtk_publish_preload_path, PRELOAD_SIGNALER)
 
 
 
